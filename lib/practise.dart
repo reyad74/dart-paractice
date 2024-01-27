26285be15c83de9b6e4e134425ad3cfcbddcbad8
Reyad <mdreyadh749@gmail.com>
2024-01-28 01:28:10 +0600

    dart loop and condition

--- lib/practise.dart
+++ lib/practise.dart
@@ -1,36 +1,8 @@
-
-
-void main() {
-  print("welcome to dart");
-  /*var map_name={
-    'key1':'value1',
-    'raju': 2,
-    'key3': 3.0,
-    'key4': true
-
-  };
-  map_name['key1'] = "Riaydh";*/
-  var mapName = Map();
-  mapName["name"] = "Riaydh";
-  mapName["address"] = "Dhaka";
-  mapName["no"] = "1654";
-  mapName["name"] = "Riaydh";
-  mapName["name"] = "Riaydh";
-  mapName["name"] = "Riaydh";
-  mapName["name"] = "Riaydh";
-
-  print(mapName.isEmpty);
-  print(mapName.isNotEmpty);
-  print(mapName.length);
-  print(mapName.keys);
-  print(mapName.values);
-  print(mapName.remove('address'));
-  print(mapName);
-
-
-
-
+main(){
+  var a = 100;
+  if(a>200){
+    //true
+  } else if(a<50){
+    //false
+  }
 }
-
-
-
