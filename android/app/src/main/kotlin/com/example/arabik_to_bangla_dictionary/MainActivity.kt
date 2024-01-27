f39ecd369b59e72042e43b628556d899513caa69
Reyad <mdreyadh749@gmail.com>
2024-01-28 01:17:32 +0600

    dart practice

---
+++ android/app/src/main/kotlin/com/example/arabik_to_bangla_dictionary/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.arabik_to_bangla_dictionary
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/practise.dart
@@ -0,0 +1,36 @@
+
+
+void main() {
+  print("welcome to dart");
+  /*var map_name={
+    'key1':'value1',
+    'raju': 2,
+    'key3': 3.0,
+    'key4': true
+
+  };
+  map_name['key1'] = "Riaydh";*/
+  var mapName = Map();
+  mapName["name"] = "Riaydh";
+  mapName["address"] = "Dhaka";
+  mapName["no"] = "1654";
+  mapName["name"] = "Riaydh";
+  mapName["name"] = "Riaydh";
+  mapName["name"] = "Riaydh";
+  mapName["name"] = "Riaydh";
+
+  print(mapName.isEmpty);
+  print(mapName.isNotEmpty);
+  print(mapName.length);
+  print(mapName.keys);
+  print(mapName.values);
+  print(mapName.remove('address'));
+  print(mapName);
+
+
+
+
+}
+
+
+
