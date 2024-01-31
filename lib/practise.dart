e7fb3fe7ca47e29db2d8f51479784d84a686ec76
Reyad <mdreyadh749@gmail.com>
2024-02-01 00:44:58 +0600

    dart loop and condition

--- lib/practise.dart
+++ lib/practise.dart
@@ -1,15 +1,3 @@
-main(){
-  var a = 100;
-  if(a>200){
-    //true
-  } else if(a<50){
-    //false
-  }
-  else if(a<58){
-
-  }else if(a>85){
-
-  }else if(a<2850){
-
-  }
+void main(){
+  print("Welcome to dart");
 }
